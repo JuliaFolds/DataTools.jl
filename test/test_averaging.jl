6f8f5c6f37294b0fb450e317864089c652e8257c
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-03 19:19:48 -0700

    Fix test_averaging.jl

--- test/test_averaging.jl
+++ test/test_averaging.jl
@@ -6,7 +6,7 @@ using DataTools
 @testset begin
     @test foldl(averaging, Filter(isodd), 1:10) == 5
     @test foldl(
-        oncol(a = averaging, b = averaging);
+        oncol(a = averaging, b = averaging),
         Map(identity),
         [(a = 1, b = 2), (a = 2, b = 3)],
     ) == (a = 1.5, b = 2.5)
