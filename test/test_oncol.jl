18e65184933e8fef84dfa217a3b308ce9e65c621
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-03 23:19:14 -0700

    Add more tests

--- test/test_oncol.jl
+++ test/test_oncol.jl
@@ -1,7 +1,10 @@
 module TestOncol
 
-using Test
 using DataTools
+using Test
+using Transducers
+
+reduce_bs1(args...; kw...) = reduce(args...; basesize = 1, kw...)
 
 @testset begin
     @test oncol(a = +, b = *)((a = 1, b = 2), (a = 3, b = 4)) == (a = 4, b = 8)
@@ -9,6 +12,14 @@ using DataTools
           (sum = 3, max = 2)
     @test oncol(:a => min, :a => max)((a_min = 2, a_max = 2), (a = 1,)) ==
           (a_min = 1, a_max = 2)
+end
+
+@testset for fold in [foldl, reduce_bs1, reduce]
+    @test fold(
+        oncol(a = +, b = averaging),
+        Filter(x -> isodd(x.a)),
+        [(a = 1, b = 7), (a = 2, b = 3), (a = 3, b = 4)],
+    ) == (a = 4, b = 5.5)
 end
 
 end  # module
