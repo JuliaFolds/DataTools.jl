0ba9b89a9aec72834ebe88255f576af976e72ef9
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-28 00:48:57 -0700

    Fix tests for Julia < 1.5

--- test/test_firstitems.jl
+++ test/test_firstitems.jl
@@ -4,13 +4,15 @@ using DataTools
 using Test
 using Transducers
 
+include("utils.jl")
+
 @testset "firstitem" begin
     @test firstitem(3:7) === 3
     @test 3:7 |> Map(x -> x + 1) |> Filter(isodd) |> firstitem == 5
 end
 
 @testset "firstitems" begin
-    @test firstitems(3:7, 2) === 3:4
+    @test firstitems(3:7, 2) ==ₜ view(3:7, 1:2)
     @test 3:7 |> Map(x -> x + 1) |> Filter(isodd) |> firstitems(2) == [5, 7]
 end
 
---
+++ test/utils.jl
@@ -0,0 +1,7 @@
+"""
+    ==ₜ(x, y)
+
+Check that _type_ and value of `x` and `y` are equal.
+"""
+==ₜ(_, _) = false
+==ₜ(x::T, y::T) where T = x == y
